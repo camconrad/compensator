f9387d145ca0670f1119b0d46a0a22639c8ee03c
camconrad <camconrad@gmail.com>
2025-03-09 13:26:25 -0600

    🔜

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
     <>
       <Head>
         <link href="https://fonts.cdnfonts.com/css/neue-haas-grotesk-display-pro" rel="stylesheet" />
-        <title>Compensator Marketplace</title>
+        <title>Home | Compensator</title>
       </Head>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center text-lg font-bold sm:items-start">
-          Compensator delegate marketplace
+          Coming Soon
         </main>
         {/* footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           2025 compensator.io
