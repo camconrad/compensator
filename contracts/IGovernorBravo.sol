3af6b7574ea03e9c2302f9dce5b2c8947d94818a
camconrad <camconrad@gmail.com>
2025-03-31 11:04:46 -0600

    enhance and compile

--- contracts/IGovernorBravo.sol
+++ contracts/IGovernorBravo.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.21;
 
 interface IGovernorBravo {
+    enum ProposalState {
+        Pending, Active, Canceled, Defeated, Succeeded, Queued, Expired, Executed
+    }
+    
+    function state(uint256 proposalId) external view returns (ProposalState);
+    
+    function proposals(uint256 proposalId) external view returns (
+        uint256 id,
+        address proposer,
+        uint256 eta,
+        uint256 startBlock,
+        uint256 endBlock,
+        uint256 forVotes,
+        uint256 againstVotes,
+        bool canceled,
+        bool executed
+    );
+    
     function castVote(uint proposalId, uint8 support) external;
 }
